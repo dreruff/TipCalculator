79e90fff374c0320a46da858957418bc64bb0f6c
dreruff <gregoryaruffin@gmail.com>
2016-12-12 22:01:50 -0800

    first comment

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +25,41 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if let name = UserDefaults.standard.object(forKey: "myName") as? String {
+            nameLabel.text = name
+        }
+    }
 
+    @IBAction func onTap(_ sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: AnyObject) {
+        
+        let tipPercentage = [0.18, 0.20, 0.25]
+        
+        let bill = Double(billField.text!) ?? 0.0
+        let tip = bill * tipPercentage[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        switch tipControl.selectedSegmentIndex {
+            case 0:
+                self.view.backgroundColor = UIColor.red
+            case 1:
+                self.view.backgroundColor = UIColor.yellow
+            case 2:
+                self.view.backgroundColor = UIColor.green
+            default:
+                self.view.backgroundColor = UIColor.white
+        }
+        
+        tipLabel.text = String(format: "%.2f", tip)
+        totalLabel.text = String(format: "%.2f", total)
+    }
+    
+    
 
 }
 
